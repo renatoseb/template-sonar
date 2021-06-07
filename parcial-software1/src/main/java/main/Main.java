3df1e584361a0910c78277774f39c0ffc3ff3662
renatoseb <renatorodriguezllanos@gmail.com>
2021-06-06 23:48:06 -0500

    add package

---
+++ parcial-software1/src/main/java/main/Main.java
@@ -0,0 +1,12 @@
+package main;
+
+import java.util.logging.Logger;
+
+public class Main {
+
+    static final Logger logger = Logger.getLogger(Main.class.getName());
+
+    public static void main(String[] args){
+        logger.info("hello world!");
+    }
+}
